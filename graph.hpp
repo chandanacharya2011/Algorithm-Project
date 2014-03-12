69a9b7ffd56bdff9d7b023a1a513a9cfd398a1e3
wny <wny1990@gmail.com>
2014-03-12 02:28:10 -0500

    03-12

--- graph.hpp
+++ graph.hpp
@@ -1,11 +1,10 @@
-
+//#include"vertex.hpp"
 template<typename key_type>
 struct adj_node
 {
-	
-	vertex<key_type> v;
+	int name;
+	key_type weight;
 	adj_node* adj_v;
-
 };
 
 //declaration
@@ -15,15 +14,19 @@ class graph{
 private:
 	int v_count; 
 	int e_count;
-	struct adj_node<key_type> v_table[GRAPH_SIZE];
-	graph();
+	struct adj_node<key_type> adj_table[GRAPH_SIZE];
+	struct vertex<key_type> v_table[GRAPH_SIZE];
 public:
-	int add_vertex(key_type key);
+	graph();
+	int add_vertex(int index, key_type key);
 //	int del_vertex(vertex<key_type> v1);
-	int add_edge(vertex<key_type> v1,vertex<key_type> v2,key_type wt);
-//	int del_edge(vertex<key_type> v1,vertex<key_type> v2);
+	int add_edge(int v1,int v2,key_type weight);
+	bool is_edge(int v1,int v2);
+	key_type edge_weight(int v1,int v2);
+//	int del_edge(int v1,int v2);
 	int get_v_count(); 
 	int get_e_count();
+	vertex<key_type> get_v(int name);
 //	int destroy_graph();
 };
 
@@ -51,19 +54,72 @@ int graph<key_type,GRAPH_SIZE>::get_e_count()
 
 
 template<typename key_type,int GRAPH_SIZE>
-int graph<key_type,GRAPH_SIZE>::add_vertex(key_type key)
+int graph<key_type,GRAPH_SIZE>::add_vertex(int v1, key_type key)
 {
-	v_table[v_count].v = key;
-	v_table[v_count].adj_v = NULL;
+	v_table[v1] = vertex<key_type>(v1,key);
+	adj_table[v1].name = v1;
+	adj_table[v1].adj_v = NULL;
 	v_count ++;
+	return 0;
 }
 
 template<typename key_type,int GRAPH_SIZE>
-int graph<key_type,GRAPH_SIZE>::add_edge(vertex<key_type> v1,vertex<key_type> v2,key_type wt)
+int graph<key_type,GRAPH_SIZE>::add_edge(int v1,int v2,key_type wt)
 {
-	adj_node node1 = new adj_node();
-	adj_node node2 = new adj_node();
-	v1.
+	adj_node<key_type>* node1 = new adj_node<key_type>();
+	node1->name = v1;
+	node1->weight = wt;
+	node1->adj_v = NULL;
+	
+	adj_node<key_type>* node2 = new adj_node<key_type>();
+	node2->name = v2;
+	node2->weight = wt;
+	node2->adj_v = NULL;
+	
+	struct adj_node<key_type>* temp;
+	
+	temp = &adj_table[v1];
+	while(temp->adj_v != NULL) temp = temp->adj_v;
+	temp->adj_v = node2;
+
+	temp = &adj_table[v2];
+	while(temp->adj_v != NULL) temp = temp->adj_v;
+	temp->adj_v = node1;
+	
+	e_count++;
+	return 0;
 }
 
+template<typename key_type,int GRAPH_SIZE>
+bool graph<key_type,GRAPH_SIZE>::is_edge(int v1,int v2)
+{
+	struct adj_node<key_type>* temp;
+	temp = &adj_table[v1];
+	while(temp != NULL)
+	{
+		if (temp->name == v2) return true;
+		//std::cout << "name: "<< temp->name <<std::endl;
+		temp = temp->adj_v;
+	}
+	return false;
+}
+
+template<typename key_type,int GRAPH_SIZE>
+key_type graph<key_type,GRAPH_SIZE>::edge_weight(int v1,int v2)
+{
+	struct adj_node<key_type>* temp;
+	temp = &adj_table[v1];
+	while(temp != NULL)
+	{
+		if (temp->name == v2) return temp->weight;
+		temp = temp->adj_v;
+	}
+	return 0;
+}
+
+template<typename key_type,int GRAPH_SIZE>
+vertex<key_type> graph<key_type,GRAPH_SIZE>::get_v(int v1)
+{
+	return v_table[v1];
+}
 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "heap.hpp"
 #include "vertex.hpp"
-//#include "graph.hpp"
+#include "graph.hpp"
 #define KEY_TYPE double
 using namespace std;
 
@@ -11,16 +11,41 @@ int main(){
 	heap<vertex<KEY_TYPE>,5000,value_fun<KEY_TYPE>,KEY_TYPE> h0;
 	
 	int i,j;
+
+	graph<KEY_TYPE,1000> g1;
+
 	vertex<KEY_TYPE> n1(1,11.2),n2(2,22),n3(3,16),n4(4,6),n5(5,126),n6(6,16),n7(7,18),n8(8,28);
 
-	h0.insert(n1);
-	h0.insert(n2);
-	h0.insert(n3);
-	h0.insert(n4);
-	h0.insert(n5);
-	h0.insert(n6);
-	h0.insert(n7);
-	h0.insert(n8);
+	g1.add_vertex(1,11.2);
+	g1.add_vertex(2,22);
+	g1.add_vertex(3,16);
+	g1.add_vertex(4,6);
+	g1.add_vertex(5,126);
+	g1.add_vertex(6,16);
+	g1.add_vertex(7,18);
+	g1.add_vertex(8,28);
+
+	g1.add_edge(2,3,2.2);
+	g1.add_edge(1,3,3);
+	g1.add_edge(4,7,4);
+
+	cout <<"vcount: " << g1.get_v_count() << endl;
+	cout <<"ecount: " << g1.get_e_count() << endl;
+	
+	cout <<"2,3: " << g1.is_edge(2,3) << " wt: "<<g1.edge_weight(2,3)<<endl;
+	cout <<"3,8: " << g1.is_edge(3,8) << " wt: "<< g1.edge_weight(3,8)<<endl;
+	cout <<"5,8: " << g1.is_edge(5,8) << " wt: "<< g1.edge_weight(5,8)<<endl;
+	cout <<"1,3: " << g1.is_edge(3,1) << " wt: "<< g1.edge_weight(3,1)<<endl;
+	cout << endl;
+
+	h0.insert(g1.get_v(1));
+	h0.insert(g1.get_v(2));
+	h0.insert(g1.get_v(3));
+	h0.insert(g1.get_v(4));
+	h0.insert(g1.get_v(5));
+	h0.insert(g1.get_v(6));
+	h0.insert(g1.get_v(7));
+	h0.insert(g1.get_v(8));
 	
 	cout <<"min:"<< h0.min().get_key() << endl;
 	cout <<"size:"<< h0.size() << endl;
@@ -33,5 +58,6 @@ int main(){
 		cout <<"after remove:"<< endl;
 		for (i=1;i<=h0.size();i++) cout << h0.index(i).get_name()<< " , "<<h0.index(i).get_key() <<endl;
 	}
+
 	return 0;
 }
